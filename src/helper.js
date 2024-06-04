754ea3e290111d1e06a50aedd2b1d9f7a72afda5
SG2048 <101252855+SG2048@users.noreply.github.com>
2024-06-04 12:17:06 +0100

    added in repeating particles
    added the bounds into calculateForce & calculatePotentialEnergy and drew particles in a repeating pattern

--- src/helper.js
+++ src/helper.js
@@ -2,7 +2,7 @@ function arrayPairs(arr) {
   return arr.map((v, i, a) => [a.at(i - 1), a.at(i)])
 }
 function calculateForce(g, s) {
-  const r = s.subtract(g.s)
+  const r = s.subtract(g.s).bounds(...screenSize.scale(1/2),...screenSize.scale(-1/2))
   let attraction;
   let repulsion;
   let effectiveDistance;
@@ -19,7 +19,7 @@ function calculateForces(a, s) {
   return a.reduce((p, c) => p.add(calculateForce(c, s)), new Vec(0, 0))
 }
 function gravitationalPotential(g, s) {
-  const r = s.subtract(g.s)
+  const r = s.subtract(g.s).bounds(...screenSize.scale(1/2),...screenSize.scale(-1/2))
   let effectiveDistance = (1/atomicRadius)*r.mag
   if (r.mag <= 1) {
     return 0
